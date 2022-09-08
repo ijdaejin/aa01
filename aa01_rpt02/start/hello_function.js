498e8f1bc1df8aa2a585686ff213baf8e80f1142
ijdaejin <90394366+ijdaejin@users.noreply.github.com>
2022-09-08 16:23:13 +0900

    Add files via upload

---
+++ aa01_rpt02/start/hello_function.js
@@ -0,0 +1,7 @@
+//hello function.js
+function hello(what) {
+    console.log("Hello " + what + " ! ");
+}
+
+hello("aa01");
+hello("ijdaejin, 강대진");
---
+++ aa01_rpt02/start/index_aa01.js
@@ -0,0 +1,7 @@
+//index_aa01.js
+
+var myinfo = require('./aa01info');
+
+myinfo("aa04", "ijdaejin", '010-73xx-xxxx');
+
+myinfo("aa01", "comsi", '010-73xx-xxxx');
